db81c95d45981a697c1ae6bc548f3d7894bf17ec
Thiam-Elhadj-Ibrahim <thiam.ibrahim@hotmail.com>
2020-11-09 17:26:43 +0100

    2e commit - 09/11/20

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,66 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'account',
+    loadChildren: () => import('./pages/account/account.module').then( m => m.AccountPageModule)
+  },
+  {
+    path: 'awards',
+    loadChildren: () => import('./pages/awards/awards.module').then( m => m.AwardsPageModule)
+  },
+  {
+    path: 'awards-about',
+    loadChildren: () => import('./pages/awards-about/awards-about.module').then( m => m.AwardsAboutPageModule)
+  },
+  {
+    path: 'forgot',
+    loadChildren: () => import('./pages/forgot/forgot.module').then( m => m.ForgotPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'location',
+    loadChildren: () => import('./pages/location/location.module').then( m => m.LocationPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./pages/orders/orders.module').then( m => m.OrdersPageModule)
+  },
+  {
+    path: 'pannier',
+    loadChildren: () => import('./pages/pannier/pannier.module').then( m => m.PannierPageModule)
+  },
+  {
+    path: 'portal',
+    loadChildren: () => import('./pages/portal/portal.module').then( m => m.PortalPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'restaurant',
+    loadChildren: () => import('./pages/restaurant/restaurant.module').then( m => m.RestaurantPageModule)
+  },
+  {
+    path: 'restaurant-about',
+    loadChildren: () => import('./pages/restaurant-about/restaurant-about.module').then( m => m.RestaurantAboutPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
+  },
 ];
 
 @NgModule({
